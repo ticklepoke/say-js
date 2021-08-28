150940eac5bdc15e4308c1d7b6a5ecb6a8a9b400
Nigel Lee <lee.nigel@hotmail.com>
2021-08-28 12:14:03 +0800

    test: Add integration test

--- tests/integration.test.ts
+++ tests/integration.test.ts
@@ -1,10 +1,31 @@
 import Driver from '@driver/driver';
+import path from 'path';
 
 describe('Integration', () => {
-	it.each([['./tests/__fixtures__/simpleCall.js', 'foo']])('%s', (pathName, expected) => {
+	it.each([
+		[
+			'./tests/__fixtures__/simpleCall.js',
+			[
+				{
+					source: {
+						file: path.resolve(__dirname, '../../tests/__fixtures__/simpleCall.js'),
+						label: 'global',
+					},
+					target: {
+						file: path.resolve(__dirname, '../../tests/__fixtures__/simpleCall.js'),
+						label: 'bar',
+					},
+				},
+			],
+		],
+	])('%s', (pathName, expected) => {
 		Driver.setFiles([pathName]);
 		const res = Driver.build();
-		console.log(res);
-		expect(true).toBe(true);
+
+		const expectation = expected.map((e) => ({
+			source: expect.objectContaining(e.source),
+			target: expect.objectContaining(e.target),
+		}));
+		expect(res).toEqual(expect.arrayContaining(expectation));
 	});
 });
