cff5a97497cb556dba1c9072067338a2819bf504
Nigel Lee <lee.nigel@hotmail.com>
2021-08-28 11:44:34 +0800

    feat: recursive stringify

--- utils/console.ts
+++ utils/console.ts
@@ -3,3 +3,19 @@ import util from 'util';
 export function prettyPrint(arg: unknown): void {
 	console.log(util.inspect(arg, false, null, true));
 }
+
+export function safeStringify(obj: any, indent = 2): string {
+	let cache: any[] | null = [];
+	const retVal = JSON.stringify(
+		obj,
+		(key, value) =>
+			typeof value === 'object' && value !== null && cache
+				? cache.includes(value)
+					? undefined // Duplicate reference found, discard key
+					: cache.push(value) && value // Store value in our collection
+				: value,
+		indent
+	);
+	cache = null;
+	return retVal;
+}
